08b69f0c489b3e371ca0986e430c22b0bde49442
Ayead Hamouch <meedinbox@gmail>
2019-10-23 19:46:39 +0100

    replaced the cors proxy

--- src/customHooks/useFetch/index.js
+++ src/customHooks/useFetch/index.js
@@ -1,10 +1,13 @@
 export const getDataApi = async (name, day = 'today') => {
   try {
-    let proxyurl = 'https://api.allorigins.win/get?url=';
+    let proxyurl = 'https://cors-dev.herokuapp.com/?q=';
     let url = `https://theastrologer-api.herokuapp.com/api/horoscope/${name}/${day}`;
     const res = await fetch(proxyurl + url);
     const json = await res.json();
-    const data = await JSON.parse(json.contents);
+    console.log(json);
+    //const parsed
+    //const data = await JSON.parse(json);
+    const data = json;
     return data;
   } catch (error) {
     return error;
